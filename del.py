07b5f4f875caac166cbc1abea39b11cb1839e555
Edwin Young <edwin.w.young@gmail.com>
2016-02-25 12:08:58 -0800

    Don't ask

--- del.py
+++ del.py
@@ -12,6 +12,7 @@ with open('README.md') as oldfile, open('newfile.txt', 'w') as newfile:
              print line
         else:
         	 newfile.write(line)
+    newfile.write("\nAdditional text inserted by Ed Young [@edwinyoung](https://www.github.com/edwinyoung)")
 
 os.remove('README.md')
 os.rename('newfile.txt','README.md')
